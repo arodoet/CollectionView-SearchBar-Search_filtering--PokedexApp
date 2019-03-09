7df1537fa07d6b2f3d6defc3838611f1b81223a5
arodoet <teodoraknezevic@gmail.com>
2019-03-09 22:18:44 +0100

    Initial Commit

---
+++ pokedex/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pokedex
+//
+//  Created by Teodora Knezevic on 3/9/19.
+//  Copyright © 2019 Teodora Knezevic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
